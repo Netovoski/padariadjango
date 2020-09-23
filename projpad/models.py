a3036b78d251c4e02f9a1ef88fc4d73d40dd0b08
unknown <netovoski@gmail.com>
2020-09-23 18:21:03 -0300

    alteração no model's Signed-off-by: unknown <netovoski@gmail.com>

--- projpad/models.py
+++ projpad/models.py
@@ -6,10 +6,6 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
