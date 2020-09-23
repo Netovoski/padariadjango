7d57ce7b9ee8e1610669df2594a965048948b38c
unknown <netovoski@gmail.com>
2020-09-23 18:30:21 -0300

    Signed-off-by: unknown <netovoski@gmail.com>

--- projpad/models.py
+++ projpad/models.py
@@ -7,15 +7,6 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-
 class TipoProduto(models.Model):
 	nmtipoproduto = models.CharField(max_length=50)
 	def __str__(self):
